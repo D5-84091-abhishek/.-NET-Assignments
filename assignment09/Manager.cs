1cf629b9a4cc54ce8cc081008686a1504241069a
Abhishek Chaudhari <caudhariabhishek61@gmail.com>
2024-05-31 17:42:35 +0530

    Completed

---
+++ assignment09/Manager.cs
@@ -0,0 +1,41 @@
+﻿using Q7;
+using Q8;
+using System.Threading.Channels;
+
+namespace Q9
+{
+    public class Manager : Employee
+    {
+		private double bonus;
+
+		public double Bonus
+		{
+			get { return bonus; }
+			set { bonus = value; }
+		}
+        public Manager()
+        {
+            Designation = "Manager";
+        }
+        public Manager(double bonus)
+        {
+            this.bonus = bonus;
+            Designation = "Manager";
+        }
+        public override void Accept()
+        {
+            base.Accept();
+            Designation = "Manager";
+            Console.Write("Enter Bonus : ");
+            Bonus = Convert.ToDouble(Console.ReadLine());
+        }
+        public override void Print()
+        {
+            Console.WriteLine("\nManager Details : "+"\n"+ToString());
+        }
+        public override string ToString()
+        {
+            return base.ToString() + ", Bonus = "+bonus+", Total Salary = "+(Salary+Bonus)+" .";
+        }
+    }
+}
