b051e5142b27f69f8d5c908143d48c82583cf6a2
Abhishek Chaudhari <caudhariabhishek61@gmail.com>
2024-05-31 17:44:53 +0530

    Completed

---
+++ assignment10/Q10/Supervisor.cs
@@ -0,0 +1,37 @@
+﻿using Q8;
+namespace Q10
+{
+    public class Supervisor : Employee
+    {
+		private int subordinates;
+		public int Subordinates
+		{
+			get { return subordinates; }
+			set { subordinates = value; }
+		}
+        public Supervisor()
+        {
+            Designation = "Supervisor";
+        }
+        public Supervisor(int subordinates)
+        {
+            this.subordinates = subordinates;
+            Designation = "Supervisor";
+        }
+        public override void Accept()
+        {
+            base.Accept();
+            Designation = "Supervisor";
+            Console.Write("Enter Subordinates : ");
+            Subordinates = Convert.ToInt32(Console.ReadLine());
+        }
+        public override void Print()
+        {
+            Console.WriteLine("\nSupervisor Details :" + "\n" + ToString());
+        }
+        public override string ToString()
+        {
+            return base.ToString()+" \nNo. of Subordinates = "+Subordinates +".";
+        }
+    }
+}
