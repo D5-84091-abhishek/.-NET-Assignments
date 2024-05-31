9669e375a4dfe2d7d8fe9f385bbca7e6c13f58ac
Abhishek Chaudhari <caudhariabhishek61@gmail.com>
2024-05-31 17:57:08 +0530

    Completed

---
+++ assignment20/Program.cs
@@ -0,0 +1,59 @@
+﻿using Question20;
+using Question8;
+using System;
+
+
+
+namespace Question20
+{
+ 
+    public class Company
+    {
+        // Other properties and methods...
+
+        // Event declaration
+        public event EventHandler EmpListChanged;
+
+        // Method to raise the event
+        protected virtual void OnEmpListChanged()
+        {
+            EmpListChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        // Method to add an employee
+        public void AddEmployee(Employee e)
+        {
+            EmpList.Add(e);
+            CalculateSalaryExpense();
+            OnEmpListChanged(); // Raise the event
+        }
+
+        // Method to remove an employee
+        public bool RemoveEmployee(int id)
+        {
+            // Remove logic
+            OnEmpListChanged(); // Raise the event
+        }
+    }
+
+  
+    internal class Program
+    {
+            static void Main()
+            {
+                Company company = new Company();
+
+                // Subscribe to the EmpListChanged event
+                company.EmpListChanged += Company_EmpListChanged;
+
+                // Other code...
+            }
+
+            // Event handler
+            private static void Company_EmpListChanged(object sender, EventArgs e)
+            {
+                // Update salary expense logic
+                Console.WriteLine("Employee list changed. Updating salary expense...");
+            }
+        }
+}
